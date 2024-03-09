9dce2c938a087225e6a92380327cd58e5825f018
bvhuiugvhjuhgcxf <ijessex42@tntech.edu>
2024-03-08 20:31:26 -0500

    More updates and fixes to classlist.sh
    More updates and fixes to classlist.sh. Also, modified classlist.txt because all of the usernames had numbers in them when they were only supposed to have alphabetic characters.

--- classlist.sh
+++ classlist.sh
@@ -10,46 +10,24 @@ fi
 
 echo -e "\nfilename: $1    username: $2"
 echo -e "Searching for $1."
-file="$1"
+FILE="$1"
+UNAME="$2"
 
-if [ ! "$(find -name "$file")" ]; then
-echo -e $file" could not be found. Please, check that the file name is enclosed in double quotes"
+if [ ! "$(find -name "$FILE")" ]; then
+echo -e $FILE" could not be found. Please, check that the file name is enclosed in double quotes"
 exit 1
 fi
 
-
 echo -e "\nSuccess"
-echo -e "\nFound "$file" at $(find -name "$file")"
+echo -e "\nFound "$FILE" at $(find -name "$FILE")"
+echo -e "Checking "$FILE" to see if "$UNAME" already exists in file."
 
+I=0
+while IFS= read -r LINE
+	do
+		((I= I + 1))
+		if [ "$UNAME" == "$LINE" ]; then
+			echo -e "Found "$UNAME" on line "$I"."
+		fi
+	done < "$(find -name "$FILE")"
 
-
-
-: << 'EOF'
-if [ ! -d "unknown" ]
-then
-    echo "unknown folder does not exist. Creating unknown."
-    mkdir ./unknown
-    echo "unknown was created."
-else
-    echo "unknown already exists."
-fi
-
-for file in *; do
-	#if[[]]; then
-		echo $file;
-	#fi
-done
-
-echo -e "System information has been gathered.  Here’s the summary:" > system_info/system_info.txt
-echo -e "\nDate and Time: $(date +%m/%d/%Y\ %T\ %Z)" >> system_info/system_info.txt
-echo -e "\nCurrent Shell User: $USER" >> system_info/system_info.txt
-echo -e "\nRoot User: $(whoami)" >> system_info/system_info.txt
-echo -e "\nCurrent Working Directory: $(pwd)" >> system_info/system_info.txt
-echo -e "\nSystem Usage:" >> system_info/system_info.txt
-echo -e "$(top -b -n1 -d5 | head -n5)" >> system_info/system_info.txt
-echo -e "\nDisk Usage:" >> system_info/system_info.txt
-echo -e "$(df -BK -H)" >> system_info/system_info.txt
-
-echo -e "Successfully gathered system infomation. Printing info from system_info.txt.\n"
-cat system_info/system_info.txt
-EOF
