37a85cd30fb5182e7834e1c6f7117c312be9a99e
bvhuiugvhjuhgcxf <ijessex42@tntech.edu>
2024-03-31 18:34:36 -0400

    Update lab10.sh
    Finished the last part. Updated problem numbers to match those of the lab

--- lab10/lab10.sh
+++ lab10/lab10.sh
@@ -15,26 +15,30 @@ echo -e "${GREEN}Found phonebook.dat. ${DEFAULT}Proceeding with script."
 GREEN='\033[0;32m'
 DEFAULT='\033[0m'
 
-#Q1
+#Q2
 echo -e "${GREEN}Starts or ends with Jose$DEFAULT"
 grep -E --color=always '^Jose|Jose$' phonebook.dat
 
-#Q2
+#Q3
 echo -e "${GREEN}Contains sequence of at least 27 upper or lower-case alphabetic characters$DEFAULT"
 grep -E --color=always '[[:alpha:]]{27}' phonebook.dat
 
-#Q3
+#Q4
 echo -e "${GREEN}Sequences of more than 18 characters$DEFAULT"
 grep -E --color=always '.{19}' phonebook.dat
 
-#Q4
+#Q5
 echo -e "${GREEN}Sequences of exactly 10 characters$DEFAULT"
 grep -E --color=always '^.{10}$' phonebook.dat
 
-#Q5
+#Q6
 echo -e "${GREEN}Sequences of 6-8 alphabetic characters surrounded by spaces or tabs$DEFAULT"
 grep -E --color=always '\s[[:alpha:]]{6,8}\s' phonebook.dat
 
-#Q6
+#Q7
 echo -e "${GREEN}Contains local phone number$DEFAULT"
 grep -E --color=always '^[0-9]{3}-[0-9]{4}|[^-d][0-9]{3}-[0-9]{4}' phonebook.dat
+
+#Q8
+echo -e "${GREEN}Contains valid URL$DEFAULT"
+grep -E --color=always '^(http|HTTP)://[a-zA-Z]+\.[a-zA-Z]+\.(com|edu)' phonebook.dat
