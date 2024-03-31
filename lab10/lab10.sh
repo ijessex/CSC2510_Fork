825f33c9941fab99d9548247b58f704dcb71f176
bvhuiugvhjuhgcxf <ijessex42@tntech.edu>
2024-03-31 17:54:26 -0400

    Update lab10.sh
    Did some debugging and finished the first two problems.

--- lab10/lab10.sh
+++ lab10/lab10.sh
@@ -1,8 +1,25 @@
-#!bin/bash
+#!/bin/bash
 
-if [ ! find ~ -type f -name phonebook.dat ]; then
+echo "$(find ~ -type f -name phonebook.dat)"
+cd ~
+
+if [ ! -e phonebook.dat ]; then
 
    echo -e "phonebook.dat cannot be found.\nExiting script."
-   exit 1;
+   exit 1
 
 fi
+
+echo -e "${GREEN}Found phonebook.dat. ${DEFAULT}Proceeding with script."
+
+GREEN='\033[0;32m'
+DEFAULT='\033[0m'
+
+#Q1
+echo -e "${GREEN}Starts or ends with Jose$DEFAULT"
+grep -E '^Jose|Jose$' phonebook.dat
+
+#Q2
+echo -e "${GREEN}Contains sequence of at least 27 upper or lower-case alphabetic characters$DEFAULT"
+grep -E '[[:alpha:]]{27}' phonebook.dat
+
